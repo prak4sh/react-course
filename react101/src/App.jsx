3b4200273442102d3c165c7a706009a000a31dad
Prakash <prakashtech250@gmail.com>
2024-08-15 16:29:06 +0545

    useState

--- react101/src/App.jsx
+++ react101/src/App.jsx
@@ -1,11 +1,25 @@
 
 import Youtube from "./Youtube"
+import { useState } from "react"
 
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  const addValue = () => {
+    setCounter(counter + 1)
+  }
+
+  function removeValue(){
+    setCounter(counter - 1)
+  }
+
   return (
     <>
       <h1>Reach Course Aug 12</h1>
-      <Youtube/>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={addValue}>Add Value</button>
+      <button onClick={removeValue}>Remove Value</button>
+      <footer>Footer: {counter}</footer>
     </>
   )
 }
