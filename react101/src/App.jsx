d8103b773d7b5ebb49e0393a1115e07c4243facf
Prakash <prakashtech250@gmail.com>
2024-08-15 20:41:49 +0545

    aug15

--- react101/src/App.jsx
+++ react101/src/App.jsx
@@ -3,23 +3,22 @@ import Youtube from "./Youtube"
 import { useState } from "react"
 
 function App() {
-  const [counter, setCounter] = useState(0)
+  const [color, setColor] = useState('cyan')
 
-  const addValue = () => {
-    setCounter(counter + 1)
-  }
-
-  function removeValue(){
-    setCounter(counter - 1)
+  const bgChange = (color) => {
+    setColor(color)
   }
 
   return (
     <>
-      <h1>Reach Course Aug 12</h1>
-      <h2>Counter Value: {counter}</h2>
-      <button onClick={addValue}>Add Value</button>
-      <button onClick={removeValue}>Remove Value</button>
-      <footer>Footer: {counter}</footer>
+      <div className="w-full h-screen duration-500" style={{background: color}}>
+        <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+          <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+            <button onClick={() => setColor('red')} className="outline-none px-4 py-2 rounded-full shadow-lg text-black bg-red-800">Red</button>
+            <button onClick={() => bgChange('green')} className="outline-none px-4 py-2 rounded-full shadow-lg text-black bg-green-800">Green</button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
